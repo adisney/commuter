2a1a3483b5e7a1a1dcf9009300f25d0a5c1df251
Alex Disney <alexdisney@gmail.com>
2015-06-27 17:19:41 -0500

    no longer storing all data files

--- commuter.sh
+++ commuter.sh
@@ -4,7 +4,7 @@ export PATH=PATH:/bin:/usr/local/bin:/usr/bin
 root_dir=`dirname $0`
 
 curl -s https://api.forecast.io/forecast/ebeafc5205b1cbee693b9089968d9ece/41.9036,-87.6733 | jq -c ".minutely.data[] | {time: (.time * 1000), probability: .precipProbability, intensity: .precipIntensity, type: .precipType}" > next_hour.json
-cp next_hour.json $root_dir/data/next_hour_`date +%s`.json
+#cp next_hour.json $root_dir/data/next_hour_`date +%s`.json
 
 curl -s -F "token=aXUVVTYmcpghSgGeCAVNxcXkXFM9fB" -F "user=uah7sDAHUfvaju3S4WvALuQK2fq5H5" https://api.pushover.net/1/messages.json -F "message=`ruby $root_dir/lib/commuter.rb`"
 printf "\n"
